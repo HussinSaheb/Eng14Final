e69f4be3028d7886f5f079b9b7a3c7b51d6181e9
Sivachel <lsivachelvan@spartaglobal.com>
2018-10-15 15:10:13 +0100

    removed instances, added as

--- modules/db_tier/main.tf
+++ modules/db_tier/main.tf
@@ -1,60 +1,4 @@
-#setting the provider in this case AWS
-provider "aws" {
-  region ="eu-central-1"
-}
-
-# 1 - terraform
-# Creating a VPC
-resource "aws_vpc" "Eng14db" {
-  cidr_block = "10.0.0.0/16"
-  tags {
-    Name = "${var.name}-vpc"
-  }
-}
-
-#Module for the DB
-module "db1" {
-  source = "./modules/db_tier"
-  vpc_id = "${aws_vpc.Eng14db.id}"
-  name = "${var.name}"
-  region = "${var.region1}"
-  ami_id = "${var.db_ami_id}"
-  app_sg = "${module.app.security_group_id}"
-  app_subnet_cidr_block = "${module.app.subnet_cidr_block}"
-}
-
-module "db2" {
-  source = "./modules/db_tier"
-  vpc_id = "${aws_vpc.Eng14db.id}"
-  name = "${var.name}"
-  region = "${var.region2}"
-  ami_id = "${var.db_ami_id}"
-  app_sg = "${module.app.security_group_id}"
-  app_subnet_cidr_block = "${module.app.subnet_cidr_block}"
-}
-
-module "db3" {
-  source = "./modules/db_tier"
-  vpc_id = "${aws_vpc.Eng14db.id}"
-  name = "${var.name}"
-  region = "${var.region3}"
-  ami_id = "${var.db_ami_id}"
-  app_sg = "${module.app.security_group_id}"
-  app_subnet_cidr_block = "${module.app.subnet_cidr_block}"
-}
-
 #DB
-#Database instance
-resource "aws_instance" "TF-DB"{
-  ami = "${var.ami_id}"
-  subnet_id ="${aws_subnet.db.id}"
-  vpc_security_group_ids = ["${var.db_sg}"]
-  instance_type = "t2.micro"
-  tags {
-    Name = "${var.name}-TF-db"
-  }
-}
-
 #Private route table for the DB
 resource "aws_route_table" "db" {
   vpc_id = "${var.vpc_id}"
@@ -67,7 +11,7 @@ resource "aws_route_table" "db" {
 #Private subnet for the DB
 resource "aws_subnet" "db1" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.3.0/24"
   availability_zone = "${var.region1}"
   map_public_ip_on_launch = false
   tags {
@@ -77,7 +21,7 @@ resource "aws_subnet" "db1" {
 
 resource "aws_subnet" "db2" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "10.0.3.0/24"
+  cidr_block = "10.0.4.0/24"
   availability_zone = "${var.region2}"
   map_public_ip_on_launch = false
   tags {
@@ -87,7 +31,7 @@ resource "aws_subnet" "db2" {
 
 resource "aws_subnet" "db3" {
   vpc_id = "${var.vpc_id}"
-  cidr_block = "10.0.4.0/24"
+  cidr_block = "10.0.5.0/24"
   availability_zone = "${var.region3}"
   map_public_ip_on_launch = false
   tags {
@@ -97,7 +41,9 @@ resource "aws_subnet" "db3" {
 
 #Route table for the private DB
 resource "aws_route_table_association" "db" {
-  subnet_ids     = ["${aws_subnet.db1.id}","${aws_subnet.db2.id}","${aws_subnet.db3.id}"]
+  subnet_ids     = ["${aws_subnet.db1.id}",
+                    "${aws_subnet.db2.id}",
+                    "${aws_subnet.db3.id}"]
   route_table_id = "${aws_route_table.db.id}"
 }
 
@@ -118,7 +64,7 @@ resource "aws_security_group" "db" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   tags {
@@ -169,7 +115,7 @@ resource "aws_launch_configuration" "db" {
   name = "${var.name}-tf-launch_configuration"
   image_id = "${var.db_ami_id}"
   instance_type = "t2.micro"
-  security_groups = ["${var.db_sg}}"]
+  security_groups = ["${aws_security_group.db.id}}"]
   user_data = "${var.user_data}"
 }
 
