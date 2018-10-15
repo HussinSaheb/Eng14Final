72bf77bc77b52afe23eaeb7c0b6af1c85ef86862
Sivachel <lsivachelvan@spartaglobal.com>
2018-10-15 14:23:40 +0100

    autoscaling for db first commit

--- modules/db_tier/variables.tf
+++ modules/db_tier/variables.tf
@@ -1,1 +1,35 @@
+variable "db_ami_id" {
+  default = "ami-0af33b09923244b93"
+}
 
+variable "name" {
+  default = "Mongod"
+}
+
+variable "db_sg" {
+  default = "${aws_security_group.db.id}"
+}
+
+variable "region1" {
+  default = "eu-central-1a"
+}
+
+variable "region2" {
+  default = "eu-central-1b"
+}
+
+variable "region3" {
+  default = "eu-central-1c"
+}
+
+variable "user_data" {
+  description = "the user data for db"
+}
+
+variable "app_sg" {
+  description = "the app security group"
+}
+
+variable "app_subnet_cidr_block" {
+  description = "the app subnet cidr block"
+}
