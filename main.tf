38b4daadb80e7bcfd4c24b40f8da6e01622fa672
Reece_Collett <rcollett@spartaglobal.com>
2018-10-17 10:50:27 +0100

    Added modules for ELK stack

--- main.tf
+++ main.tf
@@ -20,6 +20,16 @@ resource "aws_internet_gateway" "app" {
   }
 }
 
+resource "aws_subnet" "elk_stack" {
+  vpc_id = "${aws_vpc.Eng14vpc.id}"
+  cidr_block = "10.0.6.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-1a"
+  tags {
+    Name = "ELK_Stack_PubSN"
+  }
+}
+
 data "template_file" "app_init" {
    template = "${file("./scripts/app/setup.sh.tpl")}"
    vars {
@@ -42,4 +52,25 @@ module "db" {
   db_ami_id = "${var.db_ami}"
   app_sg = "${module.app.security_group_id}"
   app_subnet_cidr_block = "${module.app.subnet_cidr_block}"
+}
+
+module "elasticsearch" {
+  source "./modules/elasticsearch"
+  vpc_id = "${aws_vpc.Eng14vpc.id}"
+  ig_id = "${aws_internet_gateway.app.id}"
+  subnet_id = "${aws_subnet.elk_stack.id}"
+}
+
+module "logstash" {
+  source "./modules/logstash"
+  vpc_id = "${aws_vpc.Eng14vpc.id}"
+  ig_id = "${aws_internet_gateway.app.id}"
+  subnet_id = "${aws_subnet.elk_stack.id}"
+}
+
+module "kibana" {
+  source "./modules/kibana"
+  vpc_id = "${aws_vpc.Eng14vpc.id}"
+  ig_id = "${aws_internet_gateway.app.id}"
+  subnet_id = "${aws_subnet.elk_stack.id}"
 }
--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "app_ami" {
 variable "db_ami" {
   default = "ami-01c2032f7a7ffa4e2"
 }
+
+variable "es_ami" {
+  default = "ami-024bbdc65651e7ae5"
+}
+
+variable "ls_ami" {
+  default = ""
+}
+
+variable "kb_ami" {
+  default = "ami-085fbe53d7c21cad3"
+}
